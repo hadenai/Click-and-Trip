d936533e40b8fa616bf54d0b31b50fdbde8f4641
nateoverd12 <nathan.pousset@club.fr>
2019-06-13 17:58:44 +0200

    semantic installed homepage improved

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,11 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../scss/app.scss');
+require('semantic-ui/dist/semantic.min.css')
+require('semantic-ui/dist/semantic.min.js')
+require('../scss/_footer.scss')
+require('../scss/_navbar.scss')
+require('./_navbar.js')
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,14 @@ var Encore = require('@symfony/webpack-encore');
 Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[ext]',
+    })
+    .copyFiles({
+        from: './assets/videos',
+        to: 'videos/[path][name].[ext]',
+    })
     // public path used by the web server to access the output path
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
