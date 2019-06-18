3614d0f154a9befb77cb036538a3695e16556109
nateoverd12 <nathan.pousset@club.fr>
2019-06-18 09:54:39 +0200

    tab correct

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@ import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/pu
 const routes = require('../../public/js/fos_js_routes.json');
 Routing.setRoutingData(routes);
 
-  const [etapes, setEtapes] = useState([]);
+const [etapes, setEtapes] = useState([]);
 // any CSS you require will output into a single css file (app.css in this case)
 require('../scss/app.scss');
 require('semantic-ui/dist/semantic.min.css')
