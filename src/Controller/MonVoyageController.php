0d709f5a9d56300e4d2c7fb192c0cc4653ad2eca
Vincent Mallard <vincent.mallard@gmail.com>
2019-06-18 10:00:31 +0200

    swiftmailer in progress

--- src/Controller/MonVoyageController.php
+++ src/Controller/MonVoyageController.php
@@ -8,7 +8,6 @@
 
 namespace App\Controller;
 
-use Dompdf\Dompdf;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,9 +20,19 @@ class MonVoyageController extends AbstractController
     /**
      * @Route("/MesInfos")
      */
-    public function sendTravelerInfoMail()
+    public function sendTravelerInfoMail(\Swift_Mailer $mailer)
     {
+        $message = (new \Swift_Message('Un nouvel article vient d\'être publié !'))
+            ->setFrom('vincent.mallard5@gmail.com')
+            ->setTo('vincent.mallard5@gmail.com')
+            ->setBody($this->renderView("travelerDetailMail.html.twig",
+                [
+                    'name' => $mailer
+                ]),
+                'text/html'
+            );
 
+        $mailer->send($message);
 
         return $this->render('travelerDetailForm.html.twig');
     }
