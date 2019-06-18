dbc324698d34a330874a65175302f0bf79a06ff3
nateoverd12 <nathan.pousset@club.fr>
2019-06-18 16:24:18 +0200

    trying to use react w/ gave url

--- src/Controller/VoyageController.php
+++ src/Controller/VoyageController.php
@@ -6,7 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-
 class VoyageController extends AbstractController
 {
     /**
@@ -15,6 +14,14 @@ class VoyageController extends AbstractController
     public function index() : Response
     {
         return $this->render('homepage/index.html.twig');
+    }
+
+    /**
+     * @Route("/monVoyage/planner", name="planner")
+     */
+    public function planner() : Response
+    {
+        return $this->render('planner/listing.html.twig');
     }
 
     /**
