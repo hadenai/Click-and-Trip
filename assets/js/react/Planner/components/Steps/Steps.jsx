dd428b3f1edee6850c95b2e4505b895dd104307e
Julien Duhem <dhmjulien@gmail.com>
2019-07-02 17:09:02 +0200

    Added routing from fos

--- assets/js/react/Planner/components/Steps/Steps.jsx
+++ assets/js/react/Planner/components/Steps/Steps.jsx
@@ -46,7 +46,7 @@ function Steps() {
   };
 
   const validateTrip = () => {
-    axios.post('/mon-voyage/envoi', JSON.stringify(mySteps));
+    axios.post(Routing.generate('/mon-voyage/envoi'), JSON.stringify(mySteps));
   };
 
   const filterStepsByReference = (step, list) => {
