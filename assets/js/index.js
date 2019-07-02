38c7576ab950858a20579d7e1830f4a4c92c6993
nateoverd12 <nathan.pousset@club.fr>
2019-07-02 16:52:15 +0200

    pointer-events done, now need to force click to launch video iframe

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,3 +6,22 @@ require('semantic-ui/dist/semantic.min.js')
 require('../scss/_footer.scss')
 require('../scss/_navbar.scss')
 require('./_navbar.js')
+
+// document.getElementsByTagName('iframe').forEach(e => {
+//     console.log('before load')
+//     e.onload = () => {
+//         console.log('hey');
+//         setTimeout(function() {e.click()},4000)
+//         console.log('ho');
+//         setTimeout(function() {e.click()},8000)
+//     }
+// });
+
+const IFRAME=document.getElementsByTagName('iframe')[0];
+// IFRAME.onload(()=>{setTimeout(function() {e.click()},4000)});
+setTimeout(() => {
+    console.log("click");
+    IFRAME.click();
+    console.log("point");
+    IFRAME.style.pointerEvents="none";
+},3000);
