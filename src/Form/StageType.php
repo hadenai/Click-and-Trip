25e54781892ada0d527f99e5c2f494d030547ef7
nateoverd12 <nathan.pousset@club.fr>
2019-07-09 17:42:13 +0200

    create a priceType

--- src/Form/StageType.php
+++ src/Form/StageType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Stage;
+use App\Form\PriceType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class StageType extends AbstractType
 {
