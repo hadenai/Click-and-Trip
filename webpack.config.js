9b4c62c8ca37c409f6c630c84e1f9ecc67534553
Vincent Mallard <vincent.mallard@gmail.com>
2019-07-10 11:01:05 +0200

    adding img

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
     .enableReactPreset()
+    .addEntry("bePartner", "./assets/scss/bePartner.scss")
     .addEntry('index', './assets/js/index.js')
     .addEntry('planner', './assets/js/react/Planner/Planner.js')
     .addEntry('formRegistration', './assets/js/formRegistration.js')
